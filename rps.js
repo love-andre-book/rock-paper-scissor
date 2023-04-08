977969d3f12f369cd14e72494eb811860c4e23c8
Love Book <48724304+love-andre-book@users.noreply.github.com>
2023-04-08 14:32:10 +0200

    Add function for a game of 5 rounds

--- rps.js
+++ rps.js
@@ -25,5 +25,20 @@ function playRound(playerSelection, computerSelection) {
         return `You Lose! ${computerSelection} beats ${player}`
 
     }
-
 }
+
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+
+        let playerChoice = prompt("Rock, Paper or Scissor?")
+        let computerChoice = getComputerChoice()
+
+        console.log(`You chose ${playerChoice}.`)
+        console.log(`The computer chose ${computerChoice}.`)
+        console.log(playRound(playerChoice, computerChoice))
+
+    }
+}
+
+game()
