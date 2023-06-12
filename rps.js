d17913d2ad693c0e207ee9b0b380572a0a70a0ab
Love Book <48724304+love-andre-book@users.noreply.github.com>
2023-06-12 21:46:34 +0200

    Fix UI

--- rps.js
+++ rps.js
@@ -1,44 +1,99 @@
-function getComputerChoice() {
-    
-    let choices = ["Rock", "Paper", "Scissor"]
-    let choice = Math.floor(Math.random() * choices.length)
+const buttons = document.querySelectorAll("button");
+const msg = document.querySelector(".message");
+let playerScore = 0;
+let computerScore = 0;
 
-    return choices[choice]
+const winMessages = {
+    'Sword': 'is faster than',
+    'Shield': 'blocks',
+    'Morningstar': 'crushes'
+}
+
+
+
+buttons.forEach(btn => {
+
+    // retrieve the string representation of the attribute
+    const choice = btn.id;
+
+    btn.addEventListener('click', e => {
+        msg.textContent =  playRound(choice , getComputerChoice())
+    });
+});
+
+function isWinner(pScore, cScore) {
+
+    if (pScore === 5)
+        return ' Player wins!'
+    else if (cScore ===5)
+        return ' Computer wins'
+    else
+        return ''
 
 }
+
+function getComputerChoice() {
+    
+    let choices = ["Sword", "Shield", "Morningstar"];
+    let choice = Math.floor(Math.random() * choices.length);
+
+    return choices[choice];
+
+};
 
 function playRound(playerSelection, computerSelection) {
 
     // change player choice to the right format (No need to change computer choice)
-    let player = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
+    let player = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
     if (player === computerSelection) {
-        return "It's a tie!"
+
+        return "It's a tie!";
+
     }
-    else if (player === "Rock" && computerSelection === "Paper" || 
-    player === "Paper" && computerSelection === "Rock" || 
-    player === "Rock" && computerSelection === "Scissor") {
-        return `You Win! ${player} beats ${computerSelection}!`
+    else if (player === "Morningstar" && computerSelection === "Shield" || 
+    player === "Shield" && computerSelection === "Sword" || 
+    player === "Sword" && computerSelection === "Morningstar") {
+
+        return `You Win! ${player} ${winMessages[player]} ${computerSelection}!`;
+
     }
     else {
 
-        return `You Lose! ${computerSelection} beats ${player}`
+        return `You Lose! ${computerSelection} ${winMessages[computerSelection]} ${player}`;
 
     }
+};
+
+function isGameOver() {
+
+
+
 }
 
 function game() {
 
-    for (let i = 0; i < 5; i++) {
+    let computerWins = 0;
+    let playerWins = 0;
 
-        let playerChoice = prompt("Rock, Paper or Scissor?")
-        let computerChoice = getComputerChoice()
+    while (computerWins < 5 && playerWins < 5) {
 
-        console.log(`You chose ${playerChoice}.`)
-        console.log(`The computer chose ${computerChoice}.`)
-        console.log(playRound(playerChoice, computerChoice))
+        let playerChoice = prompt("Sword, Shield, or Morningstar?");
+        let computerChoice = getComputerChoice();
+
+        console.log(`You chose ${playerChoice}.`);
+        console.log(`The computer chose ${computerChoice}.`);
+        console.log(playRound(playerChoice, computerChoice));
+
+        // Update the score
+        if (playRound(playerChoice, computerChoice) === "You win the round!") {
+            playerWins++;
+        } else if (playRound(playerChoice, computerChoice) === "You lose the round!") {
+            computerWins++;
+        }
 
     }
-}
+};
 
-game()
+
+
